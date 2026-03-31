6c3e9415c4df6f5b5537c753be34e528f75388b1
mayank0-2 <mayank02091999@gmail.com>
2026-03-31 23:48:49 +0530

    feat: add treesitter.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,18 @@
 return {
-	"nvim-treesitter/nvim-treesitter"
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ':TSUpdate',
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	config = function()
+		require("nvim-treesitter").setup({
+			ensure_installed = { "python", "go", "lua" },
+			sync_install = false,
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+
+		})
+	end
 }
