3d0edb5b4c095e25b5fa7c37dcb080dba3c61c84
mayank0-2 <mayank02091999@gmail.com>
2026-04-02 18:49:02 +0530

    fix(treesitter): fix dependency issues

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,20 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
 	lazy = false,
 	build = ':TSUpdate',
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
+		{ "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
 	},
 	config = function()
-		require("nvim-treesitter").setup({
+		require("nvim-treesitter.configs").setup({
 			ensure_installed = { "python", "go", "lua" },
 			sync_install = false,
 			auto_install = true,
-			highlight = { enable = true },
+			highlight = { 
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
 			indent = { enable = true },
 
 		})
