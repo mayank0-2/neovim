bbbebf04e08200040fe30040e1df965abd5ba8a4
Mayank kumar <55244028+mayank0-2@users.noreply.github.com>
2026-04-04 16:58:41 +0530

    feat: fixes (#29)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,6 +31,9 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
+  defaults = {
+    version = "*", -- Use stable versions (semver tags) instead of main/master branches
+  },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
