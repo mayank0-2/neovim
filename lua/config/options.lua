594cf93ebab6cbb8658c81c4e1e2347ea797e9b6
Mayank kumar <55244028+mayank0-2@users.noreply.github.com>
2026-05-13 00:28:10 +0530

    Revert "feat: add inline errors" (#44)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,3 @@ vim.wo.relativenumber = true
 
 -- Sync clipboard between OS and Neovim
 vim.opt.clipboard = "unnamedplus"
-
--- Decrease update time so hover popups trigger faster (like VS Code)
-vim.opt.updatetime = 250
