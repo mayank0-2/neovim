781d5ff459e12b3412aa1f87223af4af92315ec4
Mayank kumar <55244028+mayank0-2@users.noreply.github.com>
2026-05-15 17:12:20 +0530

    feat(project): update tab name with project (#47)

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -6,5 +6,14 @@ return {
 	},
 	config = function(_, opts)
 		require("project_nvim").setup(opts)
+		local function update_title()
+			vim.opt.title = true
+			vim.opt.titlestring = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+		end
+		update_title()
+		vim.api.nvim_create_autocmd("DirChanged", "VimEnter", {
+			callback = update_title,
+		})
 	end,
 }
+
