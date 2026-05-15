44169438061f51d34322afc978e5481c871c6d66
mayank0-2 <mayank02091999@gmail.com>
2026-05-15 17:08:43 +0530

    feat(project): update tab name with project

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,13 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
-  config = function()
-    require("lualine").setup({
-      options = { theme = "auto" },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {},
+	config = function()
+		require("lualine").setup({
+			options = { theme = "auto" },
+			sections = {
+				lualine_c = { { "filename", path = 1 } },
+			},
+		})
+	end,
 }
