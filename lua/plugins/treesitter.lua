ff3b1f414268e58a32e7930a29a483e26526aee1
Mayank kumar <55244028+mayank0-2@users.noreply.github.com>
2026-05-26 00:25:48 +0530

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,7 +12,7 @@ return {
 		},
 	},
 	config = function()
-		require("nvim-treesitter.configs").setup({
+		require("nvim-treesitter.config").setup({
 			ensure_installed = { "python", "go", "lua", "java" },
 			sync_install = false,
 			auto_install = true,
